846e073e7a8779bdd80c09b48dcb77dc078dd0e1
xookerchang <e84991211@gmail.com>
2020-11-21 16:47:56 +0800

    chat output

---
+++ chat.py
@@ -0,0 +1,36 @@
+ 
+
+def read_file(filename):
+    lines =[]
+    with open(filename, 'r') as f:
+        for line in f:
+            lines.append(line.strip())
+        return lines
+
+def convert(lines):
+    new =[]
+    person = None  # ini value
+    for line in lines:
+        if line == 'Allen':
+            person = 'Allen'
+            continue
+
+        elif line == 'Tom':
+            person = 'Tom'
+            continue
+
+        if person:
+        	new.append(person +': '+line)
+    return new
+
+def write_file(filename, lines):
+	with open(filename, 'w') as f:
+		for line in lines:
+			f.write(line + '\n')
+
+def main():
+    lines = read_file('chat.txt')
+    lines = convert(lines)
+    write_file('output.txt' , lines)
+
+main()
